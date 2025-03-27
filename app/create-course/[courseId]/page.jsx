ef16603b73a544dbe485c66e3ded0a4a3d802cdd
Palawaththe Perera <10898607@students.plymouth.ac.uk>
2025-03-27 16:37:37 +0530

    CourseBasicInfo and CourseDetails components

--- app/create-course/[courseId]/page.jsx
+++ app/create-course/[courseId]/page.jsx
@@ -1,8 +1,43 @@
+'use client'
 
-function CourseLayout() {
+import { db } from "@/utils/db"
+import { CourseList } from "@/utils/schema"
+import { useUser } from "@clerk/nextjs"
+import { and, eq } from "drizzle-orm"
+import { useEffect, useState } from "react"
+import CourseBasicInfo from "./_components/CourseBasicInfo"
+import CourseDetails from "./_components/CourseDetails"
+
+function CourseLayout({params}) {
+
+    const {user} = useUser();
+    const [course,setcourse] = useState([]);
+
+    useEffect(()=>{
+        params && GetCourse();
+
+    },[params,user])
+
+    const GetCourse = async() =>{
+        const result = await db.select().from(CourseList)
+        .where(and(eq(CourseList.courseId, params?.courseId),
+        eq(CourseList?.createdBy, user?.primaryEmailAddress?.emailAddress)))
+
+        setcourse(result[0]);
+        console.log(result);
+    }
   return (
-    <div>
-        Course Layout
+    <div className='mt-10 px-7 md:px-20 lg:px-44'>
+        <h2 className='font-bold text-center text-2xl'>Course Layout</h2>
+
+        {/* Basic Info */}
+        <CourseBasicInfo course={course}/>
+
+        {/* Course Details */}
+        <CourseDetails course={course}/>
+
+        {/* List of Lessons */}
+
     </div>
   )
 }
