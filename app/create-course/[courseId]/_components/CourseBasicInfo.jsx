a74b1d53fc9a07b53569115746a969aa15b5bc71
Palawaththe Perera <10898607@students.plymouth.ac.uk>
2025-03-30 23:40:23 +0530

    Dashboard Course Card Updated

--- app/create-course/[courseId]/_components/CourseBasicInfo.jsx
+++ app/create-course/[courseId]/_components/CourseBasicInfo.jsx
@@ -1,9 +1,37 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useState } from "react";
 import { HiOutlinePuzzle } from "react-icons/hi";
 import EditCourseBasicInfo from "./EditCourseBasicInfo";
 
 function CourseBasicInfo({course,refreshData}) {
+
+  const [selectedFile, setSelectedFile] = useState();
+
+  // Select rhe file and upload to firebase storage
+
+  const onFileSelected = async(event) => {
+      const file = event.target.files[0];
+      // console.log(file);
+      setSelectedFile(URL.createObjectURL(file));
+
+      const fileName = Date.now() + '.jpg';
+
+      const storageRef = ref(storage,'ai-course/'+fileName)
+
+      await uploadBytes(storageRef,file).then((snapshot) => {
+        console.log("File uploaded successfully!");
+      }).then(resp =>{
+        getDownloadURL(storageRef).then(async(downloadURL) => {
+          console.log(downloadURL);
+          
+          await db.update(CourseList).set({
+            courseBanner:downloadURL
+          }).where(eq(CourseList.id,course?.id))
+        })
+      })
+  }
+
   return (
     <div className='p-10 border rounded-xl shadow-sm mt-5'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
