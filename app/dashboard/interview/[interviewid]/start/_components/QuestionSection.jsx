6a02886ebdc537c7a34769348d90b3b31c1f3d44
Palawaththe Perera <10898607@students.plymouth.ac.uk>
2025-05-18 01:59:14 +0530

    Update QuestionSection.jsx

--- app/dashboard/interview/[interviewid]/start/_components/QuestionSection.jsx
+++ app/dashboard/interview/[interviewid]/start/_components/QuestionSection.jsx
@@ -17,7 +17,7 @@ function QuestionSection({mockInterviewQuestion,activeQuestionIndex}) {
     <div className='p-5 border rounded-lg my-10'>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
             {Array.isArray(mockInterviewQuestion)&&mockInterviewQuestion.map((question,index) =>(
-                <h2 key={index} className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex == index&&'bg-red-300 text-black'}`}>Question #{index+1}</h2>
+                <h2 key={index} className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex == index&&'bg-blue-500 text-black'}`}>Question #{index+1}</h2>
             ))}
         </div>
 
